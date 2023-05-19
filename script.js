ca516cfd1fce936069b03c39e0f83c03cf3b7391
Dag Hjelm <dag.hjelm@gmail.com>
2023-05-19 12:20:34 +0200

    update code, add manifest and style

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 function countHighlighted() {
-  let selectedStr = window.getSelection().toString()
-  let words = selectedStr.replaceAll(/\s/g, ' ').split(' ')
+  let selectedStr = window.getSelection().toString();
+  let words = selectedStr.replaceAll(/\s/g, ' ')
+                         .split(' ')
+                         .filter((word) => word !== '');
 
   return {
-    wordCount: words.length,
-    charCount: selectedStr.length,
-    charCountNoSpace: selectedStr.replaceAll(/\s/g, '').length,
+    words: {id: 'words', value: words.length || 0},
+    chars: {id: 'chars', value: selectedStr.length || 0},
+    charsNoSpace: {id: 'chars-no-space',
+                   value: selectedStr.replaceAll(/\s/g, '').length || 0},
+  };
+}
+
+function fillCountTable() {
+  let count = countHighlighted();
+  for (let key in count) {
+    document.getElementById(count[key].id).innerHTML = count[key].value;
   }
 }
+
+fillCountTable();
